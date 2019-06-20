4ba6a42ee187cebdacd8f9809c6fa166d8ce9fda
Andreas Schuderer <schuderer@gmx.de>
2019-06-20 18:02:44 +0200

    Fix #21, changed log default to stdout, removed default log config file

--- launchpad/logutil.py
+++ launchpad/logutil.py
@@ -4,17 +4,23 @@ import logging
 import logging.config
 
 
-LOG_CONF_FILENAME_DEFAULT = './logging_cfg.yml'
+LOG_CONF_FILENAME_DEFAULT = ''
 LOG_CONF_FILENAME_ENV = os.environ.get('LAUNCHPAD_LOG', LOG_CONF_FILENAME_DEFAULT)
 
 
 def init_logging(filename=LOG_CONF_FILENAME_ENV):
-    logging_config = yaml.safe_load(open(filename))
-    logging.config.dictConfig(logging_config)
+    if filename == '':
+        logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+                            level=logging.DEBUG)
+    else:
+        logging_config = yaml.safe_load(open(filename))
+        logging.config.dictConfig(logging_config)
+
     logging.captureWarnings(True)
     new_logger = logging.getLogger(__name__)
-    if filename == LOG_CONF_FILENAME_DEFAULT:
-        new_logger.warning('Logging filename environment variable LAUNCHPAD_LOG not set, using default: %s',
-                            repr(LOG_CONF_FILENAME_DEFAULT))
+
+    if filename == '':
+        new_logger.warning('Logging filename environment variable LAUNCHPAD_LOG not set,'
+                           + 'using default logging configuration')
 
     return new_logger
