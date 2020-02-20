a19a7e8f5ec63f7079b16eedbe0ff63958bf74e1
Bob Platte <bobplatte@gmail.com>
2020-02-20 14:34:47 +0100

    Fix the try-catch block so that FileNotFoundError only occurs when loading the config. If the error occurs set 'conf = None' and stop script.

--- mllaunchpad/wsgi.py
+++ mllaunchpad/wsgi.py
@@ -26,27 +26,20 @@ logger = logging.getLogger(__name__)
 try:
     conf = config.get_validated_config()
 except FileNotFoundError:
-    logger.error(
-        "Config file could not be loaded. Starting the Flask application "
-        "will fail."
-    )
+    conf = None
 
     # if you change the name of the application variable, you need to
     # specify it explicitly for gunicorn: gunicorn ... launchpad.wsgi:appname
-try:
+if conf:
     application = Flask(__name__, root_path=conf["api"].get("root_path"))
     ModelApi(conf, application)
-except FileNotFoundError:
-    logger.error(
-        "Previous model could not be loaded during the the ModelApi initialization."
-    )
 
-if __name__ == "__main__":
-    logger.warning(
-        "Starting Flask debug server.\nIn production, please use a WSGI server, "
-        + "e.g. 'gunicorn -w 4 -b 127.0.0.1:5000 mllaunchpad.wsgi:application'"
-    )
-    application.run(debug=True)
+    if __name__ == "__main__":
+        logger.warning(
+            "Starting Flask debug server.\nIn production, please use a WSGI server, "
+            + "e.g. 'gunicorn -w 4 -b 127.0.0.1:5000 mllaunchpad.wsgi:application'"
+        )
+        application.run(debug=True)
 
 # To start an instance of production server with 4 workers:
 #  1. Set environment variables if required
