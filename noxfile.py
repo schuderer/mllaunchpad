2bf9b7e81f85266bd4bb7a63523e6b2113d3c158
Andreas Schuderer <schuderer@gmx.de>
2020-02-28 20:25:13 +0100

    Cleaned up build setup a little

--- noxfile.py
+++ noxfile.py
@@ -12,7 +12,27 @@ package_name = "mllaunchpad"
 my_py_ver = "3.7"
 autoformat = [package_name, "tests", "noxfile.py", "setup.py"]
 max_line_length = "79"
-min_coverage = "3"  # TODO: get to >= 90%
+min_coverage = "30"  # TODO: get to >= 90%
+
+# Skip "tests-3.7" by default as they are included in "coverage"
+nox.options.sessions = ["format", "lint", "tests-3.6", "coverage", "docs"]
+
+# TODO: Auto-parametrize dependencies from setup.py (regular mega-dep-check):
+# 1. loop over setup.py's module variable "requirements":
+# 1a. get available versions of all dependencies
+# pip install pandas==givemeallversions
+# Could not find a version that satisfies the requirement pandas==givemeallversions (from versions: 0.1, 0.2b0, 0.2b1, 0.2, 0.3.0b0, 0.3.0b2, 0.3.0, 0.4.0, 0.4.1, 0.4.2, 0.4.3, 0.5.0, 0.6.0, 0.6.1, 0.7.0rc1, 0.7.0, 0.7.1, 0.7.2, 0.7.3, 0.8.0rc1, 0.8.0rc2, 0.8.0, 0.8.1, 0.9.0, 0.9.1, 0.10.0, 0.10.1, 0.11.0, 0.12.0, 0.13.0, 0.13.1, 0.14.0, 0.14.1, 0.15.0, 0.15.1, 0.15.2, 0.16.0, 0.16.1, 0.16.2, 0.17.0, 0.17.1, 0.18.0, 0.18.1, 0.19.0rc1, 0.19.0, 0.19.1, 0.19.2, 0.20.0rc1, 0.20.0, 0.20.1, 0.20.2, 0.20.3, 0.21.0rc1, 0.21.0, 0.21.1, 0.22.0, 0.23.0rc2, 0.23.0, 0.23.1, 0.23.2, 0.23.3, 0.23.4, 0.24.0rc1, 0.24.0, 0.24.1, 0.24.2, 0.25.0rc0, 0.25.0, 0.25.1, 0.25.2, 0.25.3, 1.0.0rc0, 1.0.0, 1.0.1)
+# 1b. get two newest major versions, or if only one, two newest minor versions
+# 2. @nox.session(python=["3.6", my_py_ver])
+#    @nox.parametrize('pandas', ['1.0.1', '0.25.3'])  # somehow do this in loop
+#    ...
+#    @nox.parametrize('flask', ['1.1.1', '0.12.5'])
+#    def megatests(session, **dep_versions):
+#        """Check recent versions of dependencies for compatibility"""
+#        for dep, ver in dep_versions.items():
+#           session.install(f'dep=={ver}')
+#        session.install("-r", "requirements/test.txt")
+#        session.run("pytest", "tests", "--quiet")
 
 
 @nox.session(name="format", python=my_py_ver)
--- setup.py
+++ setup.py
@@ -17,23 +17,17 @@ requirements = [
     "pyyaml",
 ]
 
-setup_requirements = ["pytest-runner"]
-
-test_requirements = ["pytest"]
-
 setup(
     author="Andreas Schuderer",
     author_email="pypi@schuderer.net",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
-        # "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        # "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
@@ -44,14 +38,10 @@ setup(
     license="Apache License, Version 2.0",
     long_description=readme,  # + "\n\n" + history,
     long_description_content_type="text/x-rst",
-    # long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="mllaunchpad",
     name="mllaunchpad",
-    packages=find_packages(include=["mllaunchpad"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
+    packages=find_packages(),
     url="https://github.com/schuderer/mllaunchpad",
     version="0.0.7",
     zip_safe=False,
