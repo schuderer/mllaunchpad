77478b1ddbd06afc39fcea7e429865547a62ee99
Andreas Schuderer <schuderer@gmx.de>
2022-05-24 17:17:20 +0200

    Drop Python 3.6 support

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,6 @@ min_coverage = "90"
 nox.options.sessions = [
     "format",
     "lint",
-    "tests-3.6",
     "tests-3.7",
     "tests-3.9",
     "coverage",
@@ -36,7 +35,7 @@ nox.options.sessions = [
 # pip install pandas==givemeallversions
 # Could not find a version that satisfies the requirement pandas==givemeallversions (from versions: 0.1, 0.2b0, 0.2b1, 0.2, 0.3.0b0, 0.3.0b2, 0.3.0, 0.4.0, 0.4.1, 0.4.2, 0.4.3, 0.5.0, 0.6.0, 0.6.1, 0.7.0rc1, 0.7.0, 0.7.1, 0.7.2, 0.7.3, 0.8.0rc1, 0.8.0rc2, 0.8.0, 0.8.1, 0.9.0, 0.9.1, 0.10.0, 0.10.1, 0.11.0, 0.12.0, 0.13.0, 0.13.1, 0.14.0, 0.14.1, 0.15.0, 0.15.1, 0.15.2, 0.16.0, 0.16.1, 0.16.2, 0.17.0, 0.17.1, 0.18.0, 0.18.1, 0.19.0rc1, 0.19.0, 0.19.1, 0.19.2, 0.20.0rc1, 0.20.0, 0.20.1, 0.20.2, 0.20.3, 0.21.0rc1, 0.21.0, 0.21.1, 0.22.0, 0.23.0rc2, 0.23.0, 0.23.1, 0.23.2, 0.23.3, 0.23.4, 0.24.0rc1, 0.24.0, 0.24.1, 0.24.2, 0.25.0rc0, 0.25.0, 0.25.1, 0.25.2, 0.25.3, 1.0.0rc0, 1.0.0, 1.0.1)
 # 1b. get two newest major versions, or if only one, two newest minor versions
-# 2. @nox.session(python=["3.6", my_py_ver])
+# 2. @nox.session(python=["3.7", my_py_ver])
 #    @nox.parametrize('pandas', ['1.0.1', '0.25.3'])  # somehow do this in loop
 #    ...
 #    @nox.parametrize('flask', ['1.1.1', '0.12.5'])
@@ -87,7 +86,7 @@ def lint(session):
 
 
 # In Travis-CI: session selected via env vars
-@nox.session(python=["3.6", "3.7", my_py_ver, "3.9"])
+@nox.session(python=["3.7", my_py_ver, "3.9"])
 def tests(session):
     """Run the unit test suite"""
     session.install("-e", ".[test]")
